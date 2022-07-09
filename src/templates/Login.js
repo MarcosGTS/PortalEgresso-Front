a0534263648b7f27ca2728cd55b4f89b793718c0
MarcosGTS <marcosgtsodre@gmail.com>
2022-07-09 08:51:12 -0300

    add communication with backend

--- src/templates/Login.js
+++ src/templates/Login.js
@@ -1,54 +1,88 @@
-import { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../imgs/logo_ufma.png';
 import LayoutComponentes from '../components/layoutCompent/LayoutComponents';
 import "./Login.css";
+import ApiService from '../services/ApiService';
+import axios from 'axios';
 
-function Login() {
+class Login extends React.Component {
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    state = {
+        email: '',
+        senha: '',
+    }
 
-    return (
-        <LayoutComponentes>
-            <form className="login-form">
-                <span className="login-form-title">Bem Vindo!</span>
-                <span className="login-form-title">
-                    <img src={logo} alt="Jovem progaramador" />
-                </span>
+    constructor () {
+        super() 
+        this.service = new ApiService("/login");
+    }
 
-                <div className="wrap-input">
-                    <input
-                        className={email !== "" ? 'has-val input' : 'input'}
-                        type="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                    <span className="focus-input" data-placeholder="Email"></span>
-                </div>
+    login(email, senha) {
 
-                <div className="wrap-input">
-                    <input
-                        className={password !== "" ? 'has-val input' : 'input'}
-                        type="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                    <span className="focus-input" data-placeholder="password"></span>
-                </div>
+        const obj = {email, senha};
+        const headers = {
+            token: ""
+        }
 
-                <div className="container-login-form-btn">
-                    <button className="login-form-btn">Login</button>
-                </div>
+        axios.post("http://localhost:8080/login", obj, headers)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(erro => {
+                console.log(erro);
+            })
+    }
 
-                <div className="text-center">
-                    <sapn className="txt1">Não possui conta?</sapn>
-                    <Link className="txt2" to="/registro">Criar Conta</Link>
-                </div>
+    render () {
+        // const [email, setEmail] = useState("")
+        // const [password, setPassword] = useState("")
 
-            </form>
-        </LayoutComponentes>
-    );
+        return (
+            <LayoutComponentes>
+                <form className="login-form" 
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        this.login(this.state.email, this.state.senha);
+                    }}>
+
+                    <span className="login-form-title">Bem Vindo!</span>
+                    <span className="login-form-title">
+                        <img src={logo} alt="Jovem progaramador" />
+                    </span>
+    
+                    <div className="wrap-input">
+                        <input
+                            className={this.state.email !== "" ? 'has-val input' : 'input'}
+                            type="email"
+                            onChange={e => this.setState({email: e.target.value})}
+                        />
+                        <span className="focus-input" data-placeholder="Email"></span>
+                    </div>
+    
+                    <div className="wrap-input">
+                        <input
+                            className={this.state.senha !== "" ? 'has-val input' : 'input'}
+                            type="password"
+                            onChange={e => this.setState({senha: e.target.value})}
+                        />
+                        <span className="focus-input" data-placeholder="password"></span>
+                    </div>
+    
+                    <div className="container-login-form-btn">
+                        <button className="login-form-btn">Login</button>
+                    </div>
+    
+                    <div className="text-center">
+                        <span className="txt1">Não possui conta?</span>
+                        <Link className="txt2" to="/registro">Criar Conta</Link>
+                    </div>
+    
+                </form>
+            </LayoutComponentes>
+        );
+    }
+   
 }
 
 export default Login;
